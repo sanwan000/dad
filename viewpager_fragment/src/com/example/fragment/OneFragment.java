cbf5a725fae9d67a32901a000914461053581929
sanwan000 <448878898@qq.com>
2014-11-04 16:23:30 +0800

    sad
    sad

---
+++ viewpager_fragment/src/com/example/fragment/OneFragment.java
@@ -0,0 +1,19 @@
+package com.example.fragment;
+
+
+import com.example.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OneFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.one,null);
+		return view;
+	}
+}
